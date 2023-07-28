4396cb6bad87e11fbdda16b13ab7753301212745
lkas96 <63045323+lkas96@users.noreply.github.com>
2023-07-28 18:42:16 +0800

    Transport Admin - Added Placeholder Import Page
    Import to be revised.

---
+++ ScoobAdminPortal/TRANSPORTADMIN/transport-import.php
@@ -0,0 +1,61 @@
+<?php
+include 'TransportAdminController.php';
+include '../LogoutController.php';
+session_start();
+
+//VERIFY IF SYSTEMADMIN SESSION TYPE
+if ($_SESSION['type'] != "Transport Admin") {
+  header("Location: ../login.php");
+}
+
+if (isset($_POST["logout"])) {
+  $logout = new LogoutController();
+  $logout->logout();
+}
+?>
+
+<html>
+<head>
+  <title>Transport Admin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/custom.css">
+  <script src="../js/jquery-3.5.1.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/live-clock.js"></script>
+</head>
+
+<body>
+  <!--Navigation Bar-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="transport-home.php"><img src="../img/scoob-orange.svg" height="30px" alt="Toggle Navigation">&nbsp&nbsp Transport Admin - Homepage</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!--Navigation Shortcuts-->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto"><li class="nav-item"><a style="color:white;" id="current-time"></a></li></ul>
+    </div>
+	</nav>
+
+  <!-- Main Container -->
+  <div class="bodyContainer">
+    <div class="leftPanel">
+      <button class="customButton" type="button" onclick="window.location.href='transport-manage-buses.php'"> <span>Manage Buses</span></button><br><br>
+      <button class="customButton" type="button" onclick="window.location.href='transport-manage-drivers.php'"> <span>Manage Drivers</span></button><br><br>
+      <button class="customButton" type="button" onclick="window.location.href='transport-import.php'"> <span>Import Data</span></button><br><br>
+      <form method="post">
+	      <button class="logoutButton" tpe="button" name="logout">Logout</button>
+	    </form>
+    </div>
+
+    <div class="rightPanel">
+    WELCOME TO TRANSPORT ADMIN HOME!
+    What would you like to do today?
+    </div> <!-- End of RightPanel -->
+    
+  </div> <!-- End of Container -->
+</body>
+</html>
+
