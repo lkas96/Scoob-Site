4cbd9b7e24adfd0f459684c2a8f3a1e680f90d56
lkas96 <63045323+lkas96@users.noreply.github.com>
2023-08-05 08:32:41 +0800

    New Login Page

--- ScoobAdminPortal/login.php
+++ ScoobAdminPortal/login.php
@@ -13,23 +13,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $success = $userlogin->login($type, $email, $password);
 
     if ($type === "System Admin" && $success === true) {
-      echo "<script>alert('Login Successful. Welcome ". $type.".'); window.location.href = 'systemadmin/manage-applications-home.php';</script>";
+      echo "<script>alert('Login Successful. Welcome " . $type . ".'); window.location.href = 'systemadmin/manage-applications-home.php';</script>";
       exit();
     } else if ($type === "School Admin" && $success === true) {
-      echo "<script>alert('Login Successful. Welcome ". $type.".'); window.location.href = 'schooladmin/school-home.php';</script>";
+      echo "<script>alert('Login Successful. Welcome " . $type . ".'); window.location.href = 'schooladmin/school-home.php';</script>";
       exit();
     } else if ($type === "Transport Admin" && $success === true) {
-      echo "<script>alert('Login Successful. Welcome ". $type.".'); window.location.href = 'transportadmin/transport-home.php';</script>";
+      echo "<script>alert('Login Successful. Welcome " . $type . ".'); window.location.href = 'transportadmin/transport-home.php';</script>";
       exit();
     } else {
       echo "<script>alert('Invalid login Credentials or Account Status Pending.');</script>";
     }
   }
-
 }
 ?>
 
 <html>
+
 <head>
   <title>Scoob Web Portal</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -52,23 +52,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   <!--Login Form-->
   <div class="container">
-    <form method="post">
-    &nbsp&nbsp&nbsp<label>Login as: </label>
-      <select id="type" name="type" style="width:300px;" required>
-        <option default hidden selected></option>
-        <option value="System Admin">System Admin</option>
-        <option value="School Admin">School Admin</option>
-        <option value="Transport Admin">Transport Admin</option>
-      </select><br>
-        <!--<option value="TransportAdmin">Transport Admin</option>-->
-    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<label>Email: </label>
-      <input id="email" name="email" type="text" maxlength="30" style="width:300px;"><br>
-      &nbsp<label>Password: </label>
-      <input id="password" name="password" type="password" style="width:300px;"><br><br>
-      <input type="submit" id="submit" name="Login" value="Login" style="width:100px; height:30px;">
-    </form>
+    <div class="image-column">
+      <img src="img/scoobcirclesquare.png" alt="Image"><br>
+      <a href="onboarding/onboard-home.php">Don't have an account? Signup now</a>
+    </div>
+    <div class="form-column">
+      <h3>SCOOB ADMIN PORTAL</h3><br>
+      <form method="post">
+        <select id="type" name="type" required>
+          <option disabled hidden selected value="Login Type">Admin Type</option>
+          <option value="System Admin">System Admin</option>
+          <option value="School Admin">School Admin</option>
+          <option value="Transport Admin">Transport Admin</option>
+        </select>
+        <br>
+        <input id="email" name="email" type="text" maxlength="30" placeholder="Email / Username">
+        <br>
+        <input id="password" name="password" type="password" placeholder="Password">
+        <br>
+        <input type="submit" id="submit" name="Login" value="Login">
+      </form>
+    </div>
   </div>
 </body>
+
 </html>
 
 
@@ -76,22 +83,86 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 <style>
-    body {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      background-color: #ffa404;
-    }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    /* background-color: #F07c34; */
+    /* background-color: #ffa404; */
+    background-color: #FFFFFF;
+  }
 
-    .container {
-      padding: 20px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+  .container {
+    display: flex;
+    align-items: center;
+    max-width: 800px;
+    background-color: #F07c34;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
 
-    form {
-      text-align: center;
-    }
-  </style>
+  .image-column {
+    flex: 1;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: #F07c34;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .image-column a {
+    color: #ffffff;
+  }
+
+  .image-column img {
+    max-width: 100%;
+    display: block;
+  }
+
+  .form-column {
+    flex: 1;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .form-column h3 {
+    margin-top: 10px;
+    color: #ffffff; /* Change the color of the h2 to #093d65 */
+  }
+
+  .form-column label,
+  .form-column input,
+  .form-column select {
+    display: block;
+    width: 100%;
+    margin-bottom: 15px;
+  }
+
+  .form-column input[type="text"],
+  .form-column input[type="password"],
+  .form-column select {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    /* Adjust the width for wider input fields */
+    width: 300px;
+  }
+
+  .form-column input[type="submit"] {
+    padding: 10px 20px;
+    background-color: #093d65;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .form-column input[type="submit"]:hover {
+    background-color: #0056b3;
+  }
+</style>
